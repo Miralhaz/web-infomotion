97c9e22f0fcd53c1d25eb403053cc684f45118e3
gabicunha7 <gabriela.cunha@sptech.school>
2025-08-25 21:27:10 -0300

    regex e labels cadastro

---
+++ Design/regexCNPJ.js
@@ -0,0 +1,27 @@
+
+function confereRegex(){
+    const campo = document.getElementById("cnpj").value;
+
+    if(campo.includes(".")){
+        let regex = /\b\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}\b/g;
+        let formatado = campo.match(regex);
+
+        if(formatado != null){
+            return true;
+        } else {
+            return false;
+        }
+
+    } else{
+        let regex = /\d{14}/g;
+        let formatado = campo.match(regex);
+
+        if(formatado != null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
