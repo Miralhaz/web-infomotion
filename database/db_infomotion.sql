32d2d83affec49abd8d894d5b1fb43fd09e0f47b
JoaoVLira <joao.alves@sptech.school>
2025-08-26 20:55:44 -0300

    Adição da tabela cargos, relacionamento e modificação na modelagem

--- database/db_infomotion.sql
+++ database/db_infomotion.sql
@@ -5,16 +5,21 @@ use infomotion;
 create table empresa(
 	id int primary key auto_increment,
     nome varchar(40),
-    email varchar(30),
     cnpj char(14) not null
+);
+
+create table cargo(
+	id int primary key auto_increment,
+    nome varchar(25)
 );
 
 create table usuario(
 	id int primary key auto_increment,
     fk_empresa int,
     foreign key (fk_empresa) references empresa(id),
+    fk_cargo int,
+    foreign key (fk_cargo) references cargo(id),
     nome varchar(40) not null,
-    cargo varchar(25),
     senha varchar(25) not null,
     email varchar(30)
 );
@@ -37,17 +42,19 @@ create table alerta(
     descricao varchar(50)
 );
 
-/*Insert de teste*/
+insert into empresa (nome, cnpj) values
+('Uber', '12345678000199'),
+('99', '98765432000111');
 
-insert into empresa (nome, email, cnpj) values
-('Uber', 'contato@uber.com', '12345678000199'),
-('99', 'contato@99.com', '98765432000111');
+insert into cargo (nome) values
+('Gestor de Infraestrutura'),
+('Suporte de Infraestrutura');
 
-insert into usuario (fk_empresa, nome, cargo, senha, email) values
-(1, 'Carlos Silva', 'Suporte de Infraestrutura', 'senha123', 'carlos.silva@uber.com'),
-(1, 'Mariana Souza', 'Gerente de Infraestrutura', 'senha123', 'mariana.souza@uber.com'),
-(2, 'João Pereira', 'Suporte de Infraestrutura', 'senha123', 'joao.pereira@99.com'),
-(2, 'Ana Costa', 'Gerente de Infraestrutura', 'senha123', 'ana.costa@99.com');
+insert into usuario (fk_empresa, fk_cargo, nome, senha, email) values
+(1, 2, 'Carlos Silva', 'senha123', 'carlos.silva@uber.com'),
+(1, 1, 'Mariana Souza', 'senha123', 'mariana.souza@uber.com'),
+(2, 2, 'João Pereira', 'senha123', 'joao.pereira@99.com'),
+(2, 1, 'Ana Costa', 'senha123', 'ana.costa@99.com');
 
 insert into servidor (fk_empresa, nome, ip) values
 (1, 'Servidor Uber 01', '192.168.0.10'),
@@ -67,7 +74,9 @@ insert into alerta (fk_servidor, tipo, nivel, dataHora, descricao) values
 (2, 'CPU', 'Alto', '2025-08-22 10:45:00', 'Sobrecarga de processos no servidor'),
 (2, 'Memória', 'Médio', '2025-08-22 16:00:00', 'Consumo de memória acima de 70%');
 
+
 select * from empresa;
+select * from cargo;
 select * from usuario;
 select * from servidor;
 select * from alerta;
