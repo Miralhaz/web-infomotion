c8e6333f39f542add7602549148ce832e70dbaba
GabrielFurtad0 <gabriel.dsousa@sptech.school>
2025-09-17 12:35:05 -0300

    Mascara de CNPJ (banco funcionando)

--- web-data-viz/public/regexCNPJ.js
+++ web-data-viz/public/regexCNPJ.js
@@ -1,31 +1,19 @@
-function confereRegex(){
-    const campo = document.getElementById("cnpj").value;
+// Script das mascaras de texto 
+  document.getElementById('cnpj').addEventListener('input', function (e) /* e é o objeto do evento, nesse caso o input*/
+  {
+    let valor = e.target.value.replace(/\D/g, '');
+    valor = valor.replace(/^(\d{2})(\d)/, '$1.$2')
+    valor = valor.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+    valor = valor.replace(/\.(\d{3})(\d)/, '.$1/$2');
+    valor = valor.replace(/(\d{4})(\d)/, '$1-$2');
+    e.target.value = valor;
+  })
 
-    if(campo.includes(".")){
-        let regex = /\b\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}\b/g;
-        let formatado = campo.match(regex);
-
-        if(formatado != null){
-            return true;
-        } else {
-            return false;
-        }
-
-    } else{
-        let regex = /\d{14}/g;
-        let formatado = campo.match(regex);
-
-        if(formatado != null){
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-function printarTamanhoTela() {
-    console.log(innerWidth)
-    console.log(innerHeight);
-}
-
-let print = setInterval(printarTamanhoTela(), 1000)
+  document.getElementById('telefoneResponsavel_input').addEventListener('input', function(e) 
+    {
+    let valor = e.target.value.replace(/\D/g, '');
+    valor = valor.replace(/^(\d{2})(\d)/, '($1) $2')
+    valor = valor.replace(/^(\d{2})\) (\d{5})(\d)/, '($1) $2-$3');
+    valor = valor.replace(/\) (\d{5})(\d)/, ') $1-$2');
+    e.target.value = valor;
+  })
