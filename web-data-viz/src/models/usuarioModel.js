5da85f06e643a86d02c90135f59e8677678b8127
Miralhaz <lucas.silva051@sptech.school>
2025-09-24 15:23:42 -0300

    Cadastro de funcionario ainda não pronto

--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -1,3 +1,4 @@
+
 var database = require("../database/config")
 
 function autenticar(email, senha) {
@@ -13,14 +14,24 @@ function cadastrarGestor(nome, email, senha, cnpj) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cnpj);
 
     var instrucaoSql = `
-        insert into usuario (nome, email, senha, fk_empresa, cargo) select '${nome}', '${email}', '${senha}', '${id}', 1 from empresa where cnpj = '${cnpj}';
+        insert into usuario (nome, email, senha, fk_empresa, cargo) select '${nome}', '${email}', '${senha}', '${id}', 'Gestor' from empresa where cnpj = '${cnpj}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
+function cadastrarFuncionarioNaEmpresa(nome, email, senha, cargo) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cnpj);
+
+    var instrucaoSql = `
+        insert into usuario (nome, email, senha, fk_empresa, cargo) select '${nome}', '${email}', '${senha}', '${id}', '${cargo}' from empresa where cnpj = '${cnpj}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 
 module.exports = {
     autenticar,
-    cadastrarGestor
+    cadastrarGestor,
+    cadastrarFuncionarioNaEmpresa
 };
