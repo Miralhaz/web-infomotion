a8a1bd687f65f7ebdd5cdc251eaaa09c3641f51f
WillianbsilvaDev <willian.dsilva@sptech.school>
2025-10-04 18:07:59 -0300

    tela servidor e gerenciamento componentes funcionando

--- web-data-viz/src/models/componenteModel.js
+++ web-data-viz/src/models/componenteModel.js
@@ -4,7 +4,7 @@ function listarComponentes(idServidor) {
     console.log("ACESSEI O COMPONENTE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarComponentes()");
 
     var instrucaoSql = `
-        select fk_servidor, tipo, numero_serie, apelido, date_format(dt_cadastro, '%d/%m/%Y %H:%i:%s') from componentes c
+     select fk_servidor, tipo, c.numero_serie as numero_serie, c.apelido as apelido, date_format(c.dt_cadastro, '%d/%m/%Y %H:%i:%s') from componentes c
         join servidor s on c.fk_servidor = s.id
         where s.id = '${idServidor}'
         order by tipo;
