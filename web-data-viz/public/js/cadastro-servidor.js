fb8bd9ce6392d5f3e7907b8d6a66b1bfff2c1c8d
Miralhaz <lucas.silva051@sptech.school>
2025-10-05 18:10:34 -0300

    fix: Correção no front v1

--- web-data-viz/public/js/cadastro-servidor.js
+++ web-data-viz/public/js/cadastro-servidor.js
@@ -4,7 +4,7 @@ function telaCadastroServidor() {
 
 function listarServidoresPorEmpresa() {
   var idEmpresa = sessionStorage.ID_EMPRESA
-   console.log(idEmpresa)
+  console.log(idEmpresa)
 
   fetch(`/servidores/listarServidoresPorEmpresa/${idEmpresa}`)
     .then(function (resposta) {
@@ -14,30 +14,35 @@ function listarServidoresPorEmpresa() {
         resposta.json().then(function (resposta) {
           console.log("Dados recebidos: ", JSON.stringify(resposta));
 
-          let tabela = document.querySelector('table');
-          let frase = `
-                    <tr>
-                        <th style="font-weight:600"> apelido </th>
-                        <th style="font-weight:600"> IP </th>
-                    </tr>`;
+          let container = document.querySelector('.listagem-servidores');
+          let html = '';
 
           for (let i = 0; i < resposta.length; i++) {
-            frase += `
-                        <tr>
-                          <th> ${resposta[i].apelido} </th>
-                          <th> ${resposta[i].ip} </th> 
-                          <th>
-                              <a href="./tela-gerenciamento-servidor.html?id=${resposta[i].id}"> 
-                                  <img src="../assets/icon/edit-icon.png" alt="Icone de edição" class="iconeTabela"> 
-                              </a>
-                              <a onclick="excluirFuncionario(${resposta[i].id})"> 
-                                  <img src="../assets/icon/delete-icon.png" alt="Icone de excluir" class="iconeTabela"> 
-                              </a>
-                          </th>
-                        </tr>`;
+            html += `
+              <div class="card-servidor">
+                <div class="card-top">
+                  <h3>${resposta[i].apelido}</h3>
+                  <div class="acoes">
+                    <a href="./tela-gerenciamento-servidor.html?id=${resposta[i].id}">
+                      <img src="../assets/icon/edit-icon.png" alt="Editar" class="iconeTabela">
+                    </a>
+                    <a onclick="excluirFuncionario(${resposta[i].id})">
+                      <img src="../assets/icon/delete-icon.png" alt="Excluir" class="iconeTabela">
+                    </a>
+                  </div>
+                </div>
+
+                <p class="ip">IP: ${resposta[i].ip}</p>
+
+                <div class="metricas">
+                  <div><span>CPU</span><strong>13%</strong></div>
+                  <div><span>RAM</span><strong>63%</strong></div>
+                  <div><span>DISCO</span><strong>52%</strong></div>
+                </div>
+              </div>`;
           }
 
-          tabela.innerHTML = frase;
+          container.innerHTML = html;
         });
       } else {
         throw "Houve um erro ao tentar listar os servidores!";
@@ -51,53 +56,53 @@ function listarServidoresPorEmpresa() {
 
 
 function cadastrar() {
-    aguardar();
+  aguardar();
   var nomeVar = nome.value
   var ipVar = ip.value
   var idEmpresaVar = sessionStorage.ID_EMPRESA
-  
+
   if (
-      nomeVar == "" ||
-      ipVar == "" ||
-      idEmpresaVar == "" 
-    ) {      
-      finalizarAguardar("(Mensagem de erro para todos os campos em branco)");
-      return false;
-    }
-  
-    fetch("/servidores/cadastrar", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        nomeServer: nomeVar,
-        ipServer: ipVar,
-        idServer: idEmpresaVar,
-      
-      }),
-    })
-      .then(function (resposta) {
-        console.log("resposta: ", resposta);
-
-        if (resposta.ok) {
-          finalizarAguardar("Cadastro realizado com sucesso! Redirecionando para tela de login...");
-
-          setTimeout(() => {
-            window.location = "tela-servidores.html";
-          }, "2000");
-
-          limparFormulario();
-          finalizarAguardar();
-        } else {
-          throw "Houve um erro ao tentar realizar o cadastro!";
-        }
-      })
-      .catch(function (resposta) {
-        console.log(`#ERRO: ${resposta}`);
-        finalizarAguardar();
-      });
-
+    nomeVar == "" ||
+    ipVar == "" ||
+    idEmpresaVar == ""
+  ) {
+    finalizarAguardar("(Mensagem de erro para todos os campos em branco)");
     return false;
   }
+
+  fetch("/servidores/cadastrar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nomeServer: nomeVar,
+      ipServer: ipVar,
+      idServer: idEmpresaVar,
+
+    }),
+  })
+    .then(function (resposta) {
+      console.log("resposta: ", resposta);
+
+      if (resposta.ok) {
+        finalizarAguardar("Cadastro realizado com sucesso! Redirecionando para tela de login...");
+
+        setTimeout(() => {
+          window.location = "tela-servidores.html";
+        }, "2000");
+
+        limparFormulario();
+        finalizarAguardar();
+      } else {
+        throw "Houve um erro ao tentar realizar o cadastro!";
+      }
+    })
+    .catch(function (resposta) {
+      console.log(`#ERRO: ${resposta}`);
+      finalizarAguardar();
+    });
+
+  return false;
+}
 
