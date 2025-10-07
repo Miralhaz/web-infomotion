16230983f978149363cf2c23fcee0e3aa1b7c262
WillianbsilvaDev <willian.dsilva@sptech.school>
2025-10-07 00:09:14 -0300

    tela de edição de componente funcionando mas com bug

--- web-data-viz/src/controllers/componenteController.js
+++ web-data-viz/src/controllers/componenteController.js
@@ -22,7 +22,7 @@ function listarComponentes(req, res) {
         );
 }
 
-function   puxandoColunasPreenchidas(req, res) {
+function puxandoColunasPreenchidas(req, res) {
     var idComponente = req.params.idEspecifico_Componente;
     var id_Servidor = req.params.idServidor;
     var tipo = req.params.nomeTipo;
@@ -45,7 +45,55 @@ function   puxandoColunasPreenchidas(req, res) {
         );
 }
 
+
+function editarComponente(req, res) {
+    var idComponente = req.params.idEspecifico_Componente;
+    var id_Servidor = req.params.idServidor;
+
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var apelido = req.body.apelidoComponente;
+    var bodytipo = req.body.tipoComponente;
+    var unidadeMedida = req.body.unidadeMedidaComponente;
+    var parametro = req.body.parametro1Componente;
+    var statusPadrao = req.body.statusComponente;
+    // Faça as validações dos valores
+    if (apelido == undefined) {
+        res.status(400).send("Seu apelido está undefined!");
+    } else if (bodytipo == undefined) {
+        res.status(400).send("Seu tipo está undefined!");
+    } else if (unidadeMedida == undefined) {
+        res.status(400).send("Sua unidade de medida está undefined!");
+    }
+      else if (parametro == undefined) {
+        res.status(400).send("Seu parâmetro está undefined!");
+    }
+      else if (statusPadrao == undefined) {
+        res.status(400).send("Seu status padrão está undefined!");
+    }
+    else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        componenteModel.editarComponente(idComponente, id_Servidor, apelido, bodytipo, unidadeMedida, parametro, statusPadrao)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+
 module.exports = {
-  listarComponentes,
-  puxandoColunasPreenchidas
+    listarComponentes,
+    puxandoColunasPreenchidas,
+    editarComponente
 }
