b99f70a7679d41c8f7d534f0bffc1fcd4f860b08
giuliasantiagos <giulia.silva@sptech.school>
2025-10-13 20:45:00 -0300

    estilização de kpis

--- web-data-viz/public/js/dashboard.js
+++ web-data-viz/public/js/dashboard.js
@@ -44,52 +44,64 @@ function obterDadosKpi(idServidor) {
                     console.log("Dados recebidos: ", JSON.stringify(dados));
 
                     let divs = document.querySelector('.div-kpis');
+                    let div_temp = document.querySelector('.div-temp');
+
+                    //div_temp.style.backgroundColor = dados[0].temp_cpu > 60 ? '#940000' : '#C89C00';
+                    //div_temp.style.backgroundColor = dados[0].temp_disco > 60 ? '#940000' : '#C89C00';
+
                     let frase = `
-                <div class="uso-porcentagem">
+                <div class="div-dados">
                     <p>USO em % ATUAL</p>
                     <div class="div-content">
                         <div class="div-atual">
-                            <p>Atual:</p>
-                            <p>RAM: ${dados[0].uso_ram}</p>
-                            <p>CPU: ${dados[0].uso_cpu}</p>
-                            <p>DISCO: ${dados[0].uso_disco}</p>
+                            <a class="titulo">Atual:</a>
+                            <p>RAM: <a class="dados-uso">${Math.round(dados[0].uso_ram)}%</a></p>
+                            <p>CPU: <a class="dados-uso">${Math.round(dados[0].uso_cpu)}%</a></p>
+                            <p>DISCO: <a class="dados-uso">${Math.round(dados[0].uso_disco)}%</a></p>
                         </div>
 
-                        <div class="div-atual">
-                            <p>Parâmetro Máx:</p>
-                            <p>RAM: %</p>
-                            <p>CPU: %</p>
-                            <p>DISCO: %</p>
+                        <div class="div-parametro">
+                            <a class="titulo">Parâmetro Máx:</a>
+                            <p>RAM: <a class="dados-uso">%</a></p>
+                            <p>CPU: <a class="dados-uso">%</a></p>
+                            <p>DISCO: <a class="dados-uso">%</a></p>
                         </div>
                     </div>
                 </div>
 
-                <div>
+                <div class="div-dados">
                     <p>Temperatura atual:</p>
                     <div class="div-content">
-                        <div class="temp-cpu">
-                            <p>CPU</p>
-                            <p>Máx: °C</p>
-                            <p>Min: °C</p>
+                        <div class="temp-atual">
+                            <div class="div-graph"></div>
                             <div>
-                                <p>${dados[0].temp_cpu}°C</p>
+                                <p>CPU</p>
+                                <p class="titulo">Máx: °C</p>
+                                <p class="titulo">Min: °C</p>
+                                <div class="div-temp">
+                                    <p>${dados[0].temp_cpu}°C</p>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="temp-disco">
-                            <p>DISCO</p>
-                            <p>Máx: °C</p>
-                            <p>Min: °C</p>
+                        <div class="temp-atual">
                             <div>
-                                <p>${dados[0].temp_disco}°C</p>
+                                <p>DISCO</p>
+                                <p class="titulo">Máx: °C</p>
+                                <p class="titulo">Min: °C</p>
+                                <div class="div-temp">
+                                    <p>${dados[0].temp_disco}°C</p>
+                                </div>
                             </div>
+                            <div class="div-graph"></div>
                         </div>
                     </div>
                 </div>
 
-                <div class="qtd-proc">
+                <div class="div-dados">
                     <p>Quantidade total de processos</p>
-                    <div class="div-content">
+                    <div class="div-proc">
+                        <div class="div-graph"></div>
                         <p>${dados[0].qtd_processos}/1000</p>
                     </div>
                 </div>
