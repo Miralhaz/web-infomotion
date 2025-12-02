22d0255673c79791b50367361a324b1174cbe263
httpsrenan <renan.pereira@sptech.school>
2025-12-02 14:48:19 -0300

    dash near real time finalizada

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // var ambiente_processo = 'producao';
 var ambiente_processo = 'desenvolvimento';
 
@@ -59,66 +58,3 @@ app.listen(PORTA_APP, function () {
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
-=======
-// var ambiente_processo = 'producao';
-var ambiente_processo = 'desenvolvimento';
-
-var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
-// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
-// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
-
-require("dotenv").config({ path: caminho_env });
-
-var express = require("express");
-var cors = require("cors");
-var path = require("path");
-var PORTA_APP = process.env.APP_PORT;
-var HOST_APP = process.env.APP_HOST;
-
-var app = express();
-
-var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
-var servidoresRouter = require("./src/routes/servidores");
-var empresasRouter = require("./src/routes/empresas");
-var componentesRouter = require("./src/routes/componentes");
-var dashboardTemperaturaRouter = require("./src/routes/dashboardTemperatura")
-var dashboardNearRouter = require("./src/routes/dashboardNear");
-var dashboardRede = require("./src/routes/dashboardRede")
-var dashboardDisco = require("./src/routes/dashboardDisco")
-var dashboardHistorcio = require("./src/routes/dashboardHistorico");
-
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/usuarios", usuarioRouter);
-app.use("/servidores", servidoresRouter);
-app.use("/empresas", empresasRouter);
-app.use("/componentes", componentesRouter);
-app.use("/dados", dashboardTemperaturaRouter);
-app.use("/dashboardNear", dashboardNearRouter);
-app.use('/dashboardRede', dashboardRede);
-app.use('/dashboardDisco', dashboardDisco);
-app.use("/dashboardHistorico", dashboardHistorcio);
-
-app.listen(PORTA_APP, function () {
-    console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
-    \n\n\n                                                                                                 
-    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
-    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
-    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
-    \tSe .:producao:. você está se conectando ao banco remoto. \n\n
-    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
-});
->>>>>>> 1e47480475d5747859efdd537479c9763019dc67
